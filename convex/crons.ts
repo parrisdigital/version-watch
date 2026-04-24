819db5c2e2b46549a5cc0eafb7c17fd66ebed07f
Parris Digital <parrisdigital@gmail.com>
2026-04-24 07:07:23 -0400

    Use deterministic ingestion cron (#18)

--- convex/crons.ts
+++ convex/crons.ts
@@ -4,7 +4,7 @@ import { internal } from "./_generated/api";
 
 const crons = cronJobs();
 
-crons.interval("scheduled-ingestion", { hours: 4 }, internal.ingest.runScheduledIngestion, {});
+crons.cron("scheduled-ingestion", "0 0,8,12,16,20 * * *", internal.ingest.runScheduledIngestion, {});
 crons.cron("daily-deep-diff", "0 4 * * *", internal.ingest.runDeepDiff, {});
 
 export default crons;
